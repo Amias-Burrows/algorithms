a5701e6ae3dcbf32808f26aa7550a5056a037412
Amias-Burrows <amias.burrows@gmail.com>
2022-07-06 00:07:36 +0100

    Commented entire file. Added README.md
    It is now easier to understand than it was before
    
    README.md now has all the relevant bits of information in for people to
    use the programs

---
+++ README.md
@@ -0,0 +1,27 @@
+# Amias' Algorithms repo
+
+To see these projects in action download the files and test them out yourself.  Unless you just want to read the code then go ahead and look whenever.
+
+## Using
+
+To access the projects you can currently only use the browser console.  The `index.html` file includes the files in the code so you can open that and use the programs instantly.  If you're unsure how to do this here's a step by step after installing the files:
+
+- Double click on `index.html` to open your browser with this file
+- Right click the page and navigate to **Developer Tools** or click f12
+- Find `console` tab
+- Use commands given in the relevant section below
+
+## Commands
+
+### Reversing Singly Linked List
+
+The list object name is `list`
+
+- To add element
+    list.add([contents-of-current], [node-this-points-to (*null* for empty)]);
+- To list nodes in list in order
+    list.list();
+- To reverse list
+    list.reverse();
+- To remove node	**(Not working)**
+    list.remove([node-to-be-removed]);
--- linked-lists/reversing-singly.js
+++ linked-lists/reversing-singly.js
@@ -1,131 +1,277 @@
 console.log('Reversing Singly Loaded');
+
+	/*
+	 * Object class for a node.
+	 *
+	 * Constructor initialises the content and pointer to the next item in the list
+	 */
 class node {
+
 	constructor(content, next) {
+
 		this.content = content;
 		this.next = next;
-	}
 
-	edit_next(next) {
-		this.next = next;
-	}
-
-	reverse(previous) {
-		if (this.next == null) {
-			this.next = previous;
-		} else {
-			const next_item = this.next;
-			this.next = previous;
-			next_item.reverse(this);
-		}
-	}
-
-	find_previous(next) {
-		if (this.next == null) {
-			return false;
-		} else if (this.next == next) {
-			return this;
-		} else {
-			return this.next.find_previous(next);
-		}
-	}
-
-	find_current(content) {
-		if (this.content == content) {
-			return this;
-		} else if (this.next != null) {
-			return this.next.find_current(content);
-		} else {
-			return false;
-		}
-	}
-
-	list() {
-		if (this.next == null) {
-			return this.content + ' ||';
-		} else {
-			return this.content + ' -> ' + this.next.list();
-		}
-	}
-}
-
-class linked_list {
-	constructor() {
-		this.head = null;
-		this.foot = null;
 	}
 	
-	list() {
-		if (this.head != null) {
-			return this.head.list();
+		// Changes the pointer to point to 'next' variable passed from the parent function
+
+	edit_next(next) {
+		
+		this.next = next;
+
+	}
+
+		// Used recursively to swap all the pointers to point at their parent nodes
+
+	reverse(previous) {
+
+		if (this.next == null) {	// Base Case
+
+			this.next = previous;
+
 		} else {
-			return 'No items in list';
+
+			const next_item = this.next;
+			this.next = previous;
+			next_item.reverse(this);	// Recursive Line
+
 		}
 	}
+
+		// If item pointing at 'next' is needing identified this recursively searches until it finds the element which points to our item
 
 	find_previous(next) {
-		return this.head.find_previous(next);
-	}
 
-	find_current(content) {
-		return this.head.find_current(content);
-	}
+		if (this.next == null) {	// Base Case. Returns false if not found
 
-	reverse() {
-		this.head.reverse();
-		const foot = this.head;
-		this.head = this.foot;
-		this.foot = foot;
-		return 'List reversed';
-	}
+			return false;
 
-	add(content, next) {
-		if (content != null) {	// Makes sure content variable is not null
-			if (this.head != null) {	// Checks if item is first node added
-				if (next != null) {	// Checks if item is added to end of list
-					if (typeof next === 'object' && next) {
-						const previous = this.find_previous(next);
-					} else {
-						node = this.find_current(next);
-						const previous = this.find_previous(node);
-					}
-					if (previous == false) {	// If previous is null item is added to start of list
-						this.head = window['node' + content] = new node(content, next);
-						this.foot = window['node' + content];
-						return 'First Node created';
-					} else {
-						previous.edit_next(window['node' + content] = new node(content, next));
-						return 'Node created';
-					}
-				} else {
-					this.foot.next = window['node' + content] = new node(content, next);
-					this.foot = window['node' + content];
-					return 'Node created';
-				}
-			} else {
-				this.head = window['node' + content] = new node(content, next);
-				this.foot = window['node' + content];
-				return 'Node created';
-			}
-		} else {
-			return 'content cannot be null';
+		} else if (this.next == next) {	//Identifies the correct node
+
+			return this;
+
+		} else {	// Recursive part
+
+			return this.next.find_previous(next);
+
 		}
 	}
 
-	remove(node) {
-		const previous = this.head.find_current(node);
-		if (previous == false) {
-			this.head = node.next;
-			node = null;
-			return 'Node removed';
-		} else {
-			previous.edit_next(node.next);
-			if (node.next == null) {
-				this.foot = previous;
+		/*
+		 * Identifies the current element if only the content is provided
+		 *
+		 * Used mainly on 'linked_list.add' to make sure the user input doesn't break the program
+		 */
+
+	find_current(content) {
+
+		if (this.content == content) {	// Identifies the correct node
+
+			return this;
+
+		} else if (this.next != null) {	// Recursive part
+
+			return this.next.find_current(content);
+
+		} else {	// Base Case.  Returns false if not found
+
+			return false;
+
+		}
+	}
+
+		// Lists each element in order with '->' between elements and '||' at the end of the list
+	
+	list() {
+
+		if (this.next == null) {	// Base Case.  Outputs itself and the final pipes
+
+			return this.content + ' ||';
+
+		} else {	// Outputs itself and calls the child node
+
+			return this.content + ' -> ' + this.next.list();
+
+		}
+	}
+}
+
+	/*
+	 * Object class for the entire linked list.
+	 *
+	 * The object the user will interact with during use
+	 *
+	 * Constructor class initiates empty pointers.  No hardcoded list
+	 */
+
+class linked_list {
+
+	constructor() {
+
+		this.head = null;
+		this.tail = null;
+
+	}
+	
+		// Calls list function from the node identified from head pointer
+	
+	list() {
+
+		if (this.head != null) {	// Ensures head pointer identifies a node
+
+			return this.head.list();
+
+		} else {	// Catches an empty list.  Identified if the pointers in the list don't point at any nodes
+			
+			return 'No items in list';
+
+		}
+	}
+
+		// Calls 'find_previous' function in head node
+	
+	find_previous(next) {
+
+		return this.head.find_previous(next);
+
+	}
+
+		// Calls 'find_current' function in head node
+	
+	find_current(content) {
+
+		return this.head.find_current(content);
+
+	}
+
+		// Reverses the linked_list
+	
+	reverse() {
+
+			// Traverses list recursively reversing the pointers in each node
+		this.head.reverse();
+
+			// Handles the pointers of the list to make sure they point at the new head and foot elements
+		const tail = this.head;
+		this.head = this.tail;
+		this.tail = tail;
+		return 'List reversed';
+
+	}
+
+		/*
+		 * Adds node to list
+		 *
+		 * Function takes the contents of the node and an identifier for the next node.  Either the content or the object name if known
+		 */
+
+	add(content, next) {
+
+			// Makes sure content variable is not null
+
+		if (content != null) {	
+
+				// Checks if item is first node added
+
+			if (this.head != null) {
+				
+					// Checks if item is added to end of list
+
+				if (next != null) {
+
+						// Handles if given identifier for next node is content/object name/null
+
+					if (typeof next === 'object' && next) {
+
+						const previous = this.find_previous(next);
+
+					} else {
+
+						node = this.find_current(next);
+						const previous = this.find_previous(node);
+
+					}
+					
+						// If previous is null item is added to start of list as next item does not have any parent nodes to worry about
+					if (previous == false) {	
+
+						this.head = window['node' + content] = new node(content, next);
+						this.tail = window['node' + content];
+						return 'First Node created';
+
+					} else {
+
+						previous.edit_next(window['node' + content] = new node(content, next));
+						return 'Node created';
+
+					}
+
+					// Adds item to end of list and moves tail pointer across to new node
+
+				} else {
+
+					this.tail.next = window['node' + content] = new node(content, next);
+					this.tail = window['node' + content];
+					return 'Node created';
+
+				}
+
+				// Handles first node added to list
+				
+			} else {
+
+				this.head = window['node' + content] = new node(content, next);
+				this.tail = window['node' + content];
+				return 'Node created';
+
 			}
+
+			// Catches null first variable 'content'
+		} else {
+
+			return 'content cannot be null';
+
+		}
+	}
+
+		/*
+		 * Handles removing a node from list.  Takes 'content' contents
+		 *
+		 * WORK IN PROGRESS
+		 */
+	
+	remove(node) {
+
+		const previous = this.head.find_current(node);
+
+			// Handles node not found
+		if (previous == false) {
+
+			return 'No node in list matching the contents ' + node;
+
+		} else {
+
+			previous.edit_next(node.next);
+
+			if (node.next == null) {
+
+				this.tail = previous;
+
+			}
+
 			node = null;
 			return 'Node removed';
 		}
 	}
 }
+
+/*
+ * Initialises list object.
+ *
+ * Program best used in browser console currently
+ *
+ * Integration with 'node' cli application coming soon
+ */
 
 let list = new linked_list();
