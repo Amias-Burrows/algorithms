d57261691657fb7b8794437347bab47e9226f0a3
amias-burrows <amias.burrows@gmail.com>
2022-09-28 23:32:12 +0100

    Issue.  Linked List is too smart to allow loops
    I seem to have an issue in my singly linked list function but beyond
    that the function doesn't allow loops so I can't create a linked list
    loop to look through.  I can still take a list to find the recurring
    number.  Only one recurring number allowed though

---
+++ linked-lists/floyds-algorithm.js
@@ -0,0 +1,10 @@
+console.log('Floyds Tortoise & Hare Algorithm');
+
+
+let floyd = new linked_list();
+
+list.add(1, null);
+list.add(2, null);
+list.add(3, null);
+list.add(4, null);
+list.add(5, 3);
