a24e5f00af65c92daaf22f8ecb418f89715d7c07
amias-burrows <amias.burrows@gmail.com>
2022-09-28 23:08:38 +0100

    Done some more work on the factory
    I'm not entirely sure how to do anything more on it at the minute.  Gonna do the thing I came onto the computer for

--- design-patterns/factory-method.js
+++ design-patterns/factory-method.js
@@ -4,37 +4,45 @@ class Window {
 	constructor() {
 		this.width;
 		this.height;
-		this.glass;
 	}
 }
 
 class DGU_CLR extends Window {
-	constructor() {
-		this.bar_size;
-		this.bar_width;
-		this.type;
-	}
-}
-
-class DGU_PTX extends DGU_CLR {
-	constructor() {
-		this.ptx;
+	constructor(width, height, bar_colour, bar_width, type) {
+		super.width = width;
+		super.height = height;
+		this.bar_colour = bar_colour;
+		this.bar_width = bar_width;
+		this.type = type;
 	}
 }
 
 class SGU_CLR extends Window {
-	constructor(){
-		this.bevel;
-		this.radius;
-	}
-}
-
-class SGU_PTX extends SGU_CLR {
-	constructor() {
-		this.ptx;
+	constructor(width, height, bevel, radius){
+		super.width = width;
+		super.height = height;
+		this.bevel = bevel;
+		this.radius = radius;
 	}
 }
 
 class Window_Factory {
-	constructor()
+	build_window(request) {
+		let window = null;
+		switch(request[0]) {
+			case DGU_CLR:
+				window = new DGU_CLR(request[1], request[2], request[3], request[4], request[5]);
+				break;
+			case SGU_CLR:
+				window = new SGU_CLR(request[1], request[2], request[3], request[4]);
+				break;
+		}
+	}
+}
+
+class Shop {
+	constructor(request) {
+		let factory = new Window_Factory();
+		let window = factory.build_window(request)
+	}
 }
