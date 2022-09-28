a2ad8f9aa22ce09f166613f7a015d16330b742d3
amias-burrows <amias.burrows@gmail.com>
2022-09-28 20:22:12 +0100

    Realised what a factory method actually is.
    I've started adding in the different windows extending the previous types.  Next step is to create the factory

--- design-patterns/factory-method.js
+++ design-patterns/factory-method.js
@@ -1,79 +1,40 @@
 console.log('Factory Method');
 
-class Song {
-
-		/*
-		 * Handles variables held by parent and child classes
-		 */
-
-	contructor(name, lyrics, length, reference) {
-
-		this.name = name;
-		this.lyrics = lyrics;
-		this.length = length;
-		this.reference = reference;
-	}
-
-		/*
-		 * Plays the video in a seperate window
-		 */
-	play() {
-
-		if (this.reference != null) {
-
-			window.open(this.reference);
-
-		} else {
-
-			return 'No reference link set';
-		}
-	}
-
-	get_lyrics() {
-
-		if (this.lyrics != null) {
-
-			return this.lyrics;
-
-		} else {
-
-			return 'No lyrics set';
-		}
-	}
-
-	get_length() {
-
-		if (this.length != null) {
-
-			return this.length;
-
-		} else {
-
-			return "You've not told me how long the song is yet.";
-		}
+class Window {
+	constructor() {
+		this.width;
+		this.height;
+		this.glass;
 	}
 }
 
-class Music_video extends Song {
-
-	contructor(name, lyrics, length, reference, video) {
-
-		super(name, lyrics, length, reference);
-
-		this.video = video;
-	}
-
-	watch_video() {
-		
-		if (this.video != null) {
-			
-			window.open(this.video);
-
-		} else {
-
-			return 'No video link set';
-		}
+class DGU_CLR extends Window {
+	constructor() {
+		this.bar_size;
+		this.bar_width;
+		this.type;
 	}
 }
 
-let Bats = new Music_video('Bats', null, '2.25', 'https://youtube.com/watch?v=Hyxv30__Whg', 'https://youtube.com/watch?v=AiY1VHIy8hk');
+class DGU_PTX extends DGU_CLR {
+	constructor() {
+		this.ptx;
+	}
+}
+
+class SGU_CLR extends Window {
+	constructor(){
+		this.bevel;
+		this.radius;
+	}
+}
+
+class SGU_PTX extends SGU_CLR {
+	constructor() {
+		this.ptx;
+	}
+}
+
+class Window_Factory {
+	constructor()
+}
